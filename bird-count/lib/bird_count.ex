ccf974b17c42b0766de5c580edfd7e838b5a2c21
Herman Morales <nitrowitty@gmail.com>
2023-09-04 00:20:54 -0600

    recursion

---
+++ bird-count/lib/bird_count.ex
@@ -0,0 +1,28 @@
+defmodule BirdCount do
+
+  def today([]), do: nil
+
+  def today([h | _t]), do: h
+
+  def increment_day_count([]), do: [1]
+
+  def increment_day_count([h | t]), do: [h + 1 | t]
+
+  def has_day_without_birds?([]), do: false
+
+  def has_day_without_birds?(list), do: 0 in list
+
+  def total([]), do: 0
+
+  def total([h | t]), do: h + total(t)
+
+  def busy_days([]), do: 0
+
+  def busy_days([h | t]) when h >= 5 do
+    1 + busy_days(t)
+  end
+
+  def busy_days([_h | t]) do
+    busy_days(t)
+  end
+end
